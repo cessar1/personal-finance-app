1b42fa852d535ea13cbe0ed5594c5b6898924adb
Cesar Fernandez <chf05ster@gmail.com>
2025-09-07 23:55:11 -0300

    refactor: streamline codebase by updating imports and removing unnecessary semicolons

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,21 +1,22 @@
 import { EnvVarWarning } from '@/components/env-var-warning'
 import { AuthButton } from '@/components/auth-button'
-import { Hero } from '@/components/hero'
 import { hasEnvVars } from '@/lib/utils'
-// import Link from "next/link";
+import Link from 'next/link'
 
-export default function Home() {
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <div className="flex-1 w-full flex flex-col items-center">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-end p-3 px-5 text-sm">
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
           </div>
         </nav>
-        <div className="flex-1 flex flex-col gap-40 max-w-5xl p-5">
-          <Hero />
-        </div>
+        <div className="flex-1 w-full flex flex-col gap-20 p-5">{children}</div>
       </div>
     </main>
   )
